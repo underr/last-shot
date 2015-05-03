7b995c5cb6e2d749934fa259d2e5efa3f9f7fe40
underr <under@insicuri.net>
2015-05-03 14:01:24 -0300

    Clean code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var logo = "\ndb       .d8b.  .d8888. d888888b        .d8888. db   db  .d88b.  d
            "88      d8' `8b 88'  YP `~~88~~'        88'  YP 88   88 .8P  Y8. `~~88~~' \n" +
            "88      88ooo88 `8bo.      88           `8bo.   88ooo88 88    88    88    \n" +
            "88      88~~~88   `Y8b.    88    C8888D   `Y8b. 88~~~88 88    88    88    \n" +
-           "88booo. 88   88 db   8D    88           db   8D 88   88 `8b  d8'    88    \n" + 
+           "88booo. 88   88 db   8D    88           db   8D 88   88 `8b  d8'    88    \n" +
            "Y88888P YP   YP `8888Y'    YP           `8888Y' YP   YP  `Y88P'     YP    \n";
 
 // Create the bang array to detect bangs on queries
@@ -21,10 +21,10 @@ var index = function *() {
   this.body = logo + "\nPaths:\n * /list - list all bangs\n * /search - self-explanatory";
 }
 
-var search =  function *() {
+var search = function *() {
   var query = this.query.q;
-  var words = query.split(' '); // what about '  '? 
-  
+  var words = query.split(' '); // what about '  '?
+
   // unwanted double loop; can't be helped
   for(var i=0; i < words.length; i++) {
     for(var n=0; n < bangList.length; n++) {
@@ -35,7 +35,7 @@ var search =  function *() {
         };
     }
   }
-  
+
   if (result) {
     this.redirect(bangs[result.bangN]['url'].replace('%s', result.query));
   } else {
